6b5bd5bb010bb28cbacf9595a14c01404b3c7c0e
mmolefe023 <mmolefe023@student.wethinkcode.co.za>
2024-12-06 07:15:44 +0200

    Mock authetication, waiting for login endpoint

--- Storage-Nook/admin/script.js
+++ Storage-Nook/admin/script.js
@@ -1,19 +1,28 @@
 document.getElementById('admin-login-form').addEventListener('submit', function (event) {
   event.preventDefault();
 
-  // Get form values
+
   const email = document.getElementById('admin-email').value;
   const password = document.getElementById('admin-password').value;
 
+  // loading spinner
+  const loadingSpinner = document.getElementById('loading-spinner');
+  loadingSpinner.style.display = 'block';
+
   // Mock authentication 
-  if (email === 'admin@example.com' && password === 'password123') {
-    alert('Login successful!');
-    // Redirect to the admin dashboard 
-    window.location.href = "dashboard.html";
-  } else {
-    // Display the error message in the div
-    const errorMessage = document.getElementById('error-message');
-    errorMessage.textContent = 'Invalid email or password!';
-    errorMessage.style.display = 'block';
-  }
+  setTimeout(() => {  //mock api call
+    if (email === 'admin@example.com' && password === 'password123') {
+      alert('Login successful!');
+      // Redirect to the admin dashboard 
+      window.location.href = "dashboard.html";
+    } else {
+      // Hide the loading spinner
+      loadingSpinner.style.display = 'none';
+      
+      // Display the error message in the div
+      const errorMessage = document.getElementById('error-message');
+      errorMessage.textContent = 'Invalid email or password!';
+      errorMessage.style.display = 'block';
+    }
+  }, 1000); // Simulate a 1 second delay
 });
